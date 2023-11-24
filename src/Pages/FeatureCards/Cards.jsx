6363b3df7ff8d94b3f06622f016d60ea32a421e4
Sahil Abrar <zeroxkhan2@gmail.com>
2023-11-24 22:50:23 +0600

    added user funtionality

--- src/Pages/FeatureCards/Cards.jsx
+++ src/Pages/FeatureCards/Cards.jsx
@@ -1,17 +1,31 @@
-// eslint-disable-next-line react/prop-types
+import 'aos/dist/aos.css';
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../Providers/AuthProvider';
+
 const Cards = ({ feature }) => {
+  const { user } = useContext(AuthContext);
   const { img, name } = feature || {};
-  console.log(feature);
+  const navigate = useNavigate();
+
+  const handleVote = () => {
+    if (!user) {
+      navigate('/login');
+    }
+  };
+
   return (
     <div>
-      <div className="card shadow-xl">
+      <div data-aos="zoom-in-right" className="card shadow-xl">
         <figure>
           <img className="w-[400px] h-[300px]" src={img} alt="Shoes" />
         </figure>
         <div className="card-body">
           <h2 className="card-title text-white">{name}</h2>
           <div className="card-actions justify-end">
-            <button className="btn btn-primary">Buy Now</button>
+            <button onClick={handleVote} className="btn btn-primary">
+              Buy Now
+            </button>
           </div>
         </div>
       </div>

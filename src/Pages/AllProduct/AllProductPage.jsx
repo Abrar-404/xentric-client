2887fcb4cb8e663e4ce1a4414ffe0ac6c04a549c
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-02 10:00:40 +0600

    added btn icon

--- src/Pages/AllProduct/AllProductPage.jsx
+++ src/Pages/AllProduct/AllProductPage.jsx
@@ -1,5 +1,6 @@
 import '../../Components/Styles/allproductcard.css';
 import { Link } from 'react-router-dom';
+import '../../Components/Styles/productsbutton.css';
 
 // eslint-disable-next-line react/prop-types
 const AllProductPage = ({ item }) => {
@@ -17,9 +18,51 @@ const AllProductPage = ({ item }) => {
                 price ${price}
               </p>
             </Link>
-            <div>
+            <div className="mt-5 flex justify-center">
               <Link to={`/addProducts/${_id}`}>
-                <button className="btn">Add to cart</button>
+                <button class="buttuuu">
+                  <span>Add to cart</span>
+                  <svg
+                    className="z-50 svgicon"
+                    fill="#fff"
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                  >
+                    <g stroke-width="0" id="SVGRepo_bgCarrier"></g>
+                    <g
+                      stroke-linejoin="round"
+                      stroke-linecap="round"
+                      id="SVGRepo_tracerCarrier"
+                    ></g>
+                    <g id="SVGRepo_iconCarrier">
+                      {' '}
+                      <defs> </defs>{' '}
+                      <g id="cart">
+                        {' '}
+                        <circle
+                          r="1.91"
+                          cy="20.59"
+                          cx="10.07"
+                          class="cls-1"
+                        ></circle>{' '}
+                        <circle
+                          r="1.91"
+                          cy="20.59"
+                          cx="18.66"
+                          class="cls-1"
+                        ></circle>{' '}
+                        <path
+                          d="M.52,1.5H3.18a2.87,2.87,0,0,1,2.74,2L9.11,13.91H8.64A2.39,2.39,0,0,0,6.25,16.3h0a2.39,2.39,0,0,0,2.39,2.38h10"
+                          class="cls-1"
+                        ></path>{' '}
+                        <polyline
+                          points="7.21 5.32 22.48 5.32 22.48 7.23 20.57 13.91 9.11 13.91"
+                          class="cls-1"
+                        ></polyline>{' '}
+                      </g>{' '}
+                    </g>
+                  </svg>
+                </button>
               </Link>
             </div>
           </div>

656842460474cec04c2ee7c9e18951ee5464692d
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-02 09:43:15 +0600

    added text  margins

--- src/Pages/AllProduct/AllProductPage.jsx
+++ src/Pages/AllProduct/AllProductPage.jsx
@@ -8,42 +8,20 @@ const AllProductPage = ({ item }) => {
   return (
     <>
       <div>
-        {/* <div className="card card-compact shadow-xl">
-          <figure>
-            <img
-              className="w-[300px] h-[200px] rounded-full transform hover:rotate-45 transition duration-300 ease-in-out"
-              src={img}
-              alt="Shoes"
-            />
-          </figure>
-          <div className="card-body">
-            <Link to={`/allItem/${_id}`}>
-              <h2 className="card-title text-white font-share">{name}</h2>
-              <h2 className="card-title text-white font-share">
-                Price: ${price}
-              </h2>
-            </Link>
-            <div className="card-actions justify-start">
-              <button className="btn button-85">Upvote</button>
-            </div>
-            <div className="mt-10 w-full">
-              <Link to={`/addProducts/${_id}`}>
-                <button className="btn button-85 w-full">Add Products</button>
-              </Link>
-            </div>
-          </div>
-        </div> */}
-
-        <div class="cardish">
+        <div class="cardish mx-auto gap-10">
           <img className="w-[350px] h-[300px]" src={img} alt="" />
           <div class="cardish__content">
             <Link to={`/allItem/${_id}`}>
-              <p class="cardish__title">{name}</p>
-              <p class="cardish__description">price ${price}</p>
+              <p class="cardish__title text-center mt-10">{name}</p>
+              <p class="cardish__description mt-5 text-center">
+                price ${price}
+              </p>
             </Link>
-            <Link to={`/addProducts/${_id}`}>
-              <button className="btn">Add to cart</button>
-            </Link>
+            <div>
+              <Link to={`/addProducts/${_id}`}>
+                <button className="btn">Add to cart</button>
+              </Link>
+            </div>
           </div>
         </div>
       </div>

40a208edc70c8cfec3a029dd4d2f776a262194d8
Sahil Abrar <zeroxkhan2@gmail.com>
2024-02-04 22:13:56 +0600

    added file uploading system

--- src/Components/FileUpload/FileUpload.jsx
+++ src/Components/FileUpload/FileUpload.jsx
@@ -1,62 +1,4 @@
-// import React, { useState } from 'react';
-
-// const FileUpload = () => {
-//   const [selectedFile, setSelectedFile] = useState(null);
-
-//   const handleFileChange = event => {
-//     setSelectedFile(event.target.files[0]);
-//   };
-
-//   const handleUpload = () => {
-//     // You can perform upload logic here using selectedFile
-//     if (selectedFile) {
-//       console.log('Uploading file:', selectedFile);
-//       // Add your upload logic here (e.g., using FormData and a fetch request)
-//     } else {
-//       console.error('No file selected');
-//     }
-//   };
-
-//   const handleDownload = () => {
-//     // You can perform download logic here using selectedFile
-//     if (selectedFile) {
-//       console.log('Downloading file:', selectedFile);
-//       // Add your download logic here (e.g., creating a download link)
-//       const downloadLink = URL.createObjectURL(selectedFile);
-//       const a = document.createElement('a');
-//       a.href = downloadLink;
-//       a.download = selectedFile.name;
-//       document.body.appendChild(a);
-//       a.click();
-//       document.body.removeChild(a);
-//     } else {
-//       console.error('No file to download');
-//     }
-//   };
-
-//   return (
-//     <div className='mt-48'>
-//       {/* <input type="file" className="text-white" onChange={handleFileChange} /> */}
-//       <div className="flex justify-center mb-10">
-//         <input
-//           type="file"
-//           className="file-input file-input-bordered file-input-secondary w-full max-w-xs"
-//           onChange={handleFileChange}
-//         />
-//       </div>
-//       <div className="flex justify-center gap-10">
-//         <button className="btn" onClick={handleUpload}>
-//           Upload
-//         </button>
-//         <button className="btn" onClick={handleDownload}>
-//           Download
-//         </button>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default FileUpload;
+import '../Styles/button48.css';
 
 import React, { useState } from 'react';
 
@@ -99,7 +41,7 @@ const FileUpload = () => {
       <div className="flex justify-center mb-10">
         <input
           type="file"
-          className="file-input file-input-bordered file-input-secondary w-full max-w-xs"
+          className="file-input file-input-bordered file-input-secondary w-full max-w-xs "
           onChange={handleFileChange}
         />
       </div>
@@ -109,10 +51,10 @@ const FileUpload = () => {
         </div>
       )}
       <div className="flex justify-center gap-10">
-        <button className="btn" onClick={handleUpload}>
+        <button className="button-57 text-white" onClick={handleUpload}>
           Upload
         </button>
-        <button className="btn" onClick={handleDownload}>
+        <button className="button-57 text-white" onClick={handleDownload}>
           Download
         </button>
       </div>
